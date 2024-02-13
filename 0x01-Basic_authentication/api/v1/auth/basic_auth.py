7ccc7cf1494899e155714d4b1a7794d208b568df
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-13 16:37:14 +0300

    8.Basic- Base 64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ a module that contains class BasicAuth
 it inherits from Auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -28,3 +29,24 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """
+        returns the decoded base64 string
+        """
+        parameter_checker = [
+                             base64_authorization_header is not None,
+                             isinstance(base64_authorization_header, str)
+                             ]
+        if False in parameter_checker:
+            return None
+
+        decoded_value = None
+        try:
+            decoded_value_bytes = base64.b64decode(base64_authorization_header)
+            decoded_value = decoded_value_bytes.decode('utf-8')
+        except Exception:
+            return None
+
+        return decoded_value

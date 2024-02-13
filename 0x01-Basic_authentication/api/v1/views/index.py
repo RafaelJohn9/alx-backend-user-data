5e3821cae984d3497f1d4038bbd8762c65f823d1
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-13 12:53:48 +0300

    1.error handler unauthorizes

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> None:
+    """
+    it is used to test unauthorized
+    access """
+    abort(401)

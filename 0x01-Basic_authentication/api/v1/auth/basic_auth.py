eb0872c6535ca28f1be97116634333d7228a0fb6
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-13 15:25:39 +0300

    6.Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+a module that contains class BasicAuth
+it inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Basic Auth class
+    """
+    pass

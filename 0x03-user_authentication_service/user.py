db7f727f087a6e2486bfa8bfbc28659c07d4535a
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-20 20:25:03 +0300

    0.user model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+a model named User for a database
+table named users (by using the mapping declaration of SQLAlchemy)
+The model will have the following attributes:
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    This is a user authentication table
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)

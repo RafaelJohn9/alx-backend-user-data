8fc60c203166d849353bc43cba345bdf55f73193
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-20 22:08:03 +0300

    2.find_user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
 
 from user import Base, User
 
@@ -43,3 +46,17 @@ class DB:
         except SQLAlchemyError as e:
             session.rollback()
             raise e
+
+    def find_user_by(self, **kwargs):
+        """
+        finds user through key-word filter
+        """
+        session = self.__session
+        table = session.query(User)
+        try:
+            query = table.filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound
+        except InvalidRequestError:
+            raise InvalidRequestError
+        return query

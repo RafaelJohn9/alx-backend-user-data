a6ec4102e7305a6f508c95a35fe6e980cd5c8c99
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-20 21:25:41 +0300

    removed echo=True in my db pyfile

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -15,7 +15,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -35,6 +35,10 @@ class DB:
         """
         session = self._session
         new_user = User(email=email, hashed_password=hashed_password)
-        session.add(new_user)
-        session.commit()
-        return (new_user)
+        try:
+            session.add(new_user)
+            session.commit()
+            return (new_user)
+        except SQLAlchemyError as e:
+            session.rollback()
+            raise e

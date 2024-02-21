ec9a32ef3da0bd74d931dc4a80640a2d27904160
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-22 01:34:45 +0300

    13.Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,3 +85,6 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except (NoResultFound, InvalidRequestError):
             return None
+
+    def  destroy_session(self, user_id: int) -> None:
+        self._db.update_user(user_id, session_id=None)

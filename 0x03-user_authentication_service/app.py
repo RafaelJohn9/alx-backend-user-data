2e9fd2096b3a9eda42d31f9d70252c88e432beda
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-21 23:50:34 +0300

    7.Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,15 +3,29 @@
 A flask app that has a single  GET route '/'
 and use flask.jsonify to return a JSON payload of the form
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
 def welcome():
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """
+    it is used to register a new user
+    """
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        AUTH.register_user(email, password)
+        return {"email": email, "message": "user created"}
+    except ValueError:
+        return {'message': 'email already registered'}, 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

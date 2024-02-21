ba50991b67a47429a60a70d5ca693996f1903051
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-22 01:18:08 +0300

    corrected method by adding self as arg

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,7 +71,7 @@ class Auth:
             return None
         return session_id
 
-    def get_user_from_session_id(session_id: str) -> User:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """
         gets the user through a session id
         """

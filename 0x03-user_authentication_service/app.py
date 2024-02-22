5f8a5f43f8f3303540c49bb2bcf713dffe0acee9
RafaelJohn9 <rafaeljohb@gmail.com>
2024-02-23 02:00:08 +0300

    added method put in api route in app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -92,7 +92,7 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route('/reset_password', strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     """
     it is used to update the user's password
